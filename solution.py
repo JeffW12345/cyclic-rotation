bcd9e4272c264d5d2627b91317a94ec9ab07dc18
JeffW12345 <59927061+JeffW12345@users.noreply.github.com>
2021-10-13 12:53:09 +0100

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,23 @@
+# Python solution
+
+def solution(A, K):
+    if len(A) == 0:
+        return A
+    array_of_nums = A
+    num_rotations = K
+    num_rotations = num_rotations % len(array_of_nums)
+    for i in range(num_rotations):
+        array_of_nums = move_by_one(array_of_nums)
+    return array_of_nums
+
+
+def move_by_one(arr):
+    final_entry = arr[-1]
+    del arr[-1]
+    arr.insert(0, final_entry)
+    return arr
+
+
+A = [1, 2, 3, 4]
+K = 5
+print(solution(A, K))  # Should be [4, 1, 2, 3]
